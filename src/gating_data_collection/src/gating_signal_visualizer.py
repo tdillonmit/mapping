e3a3871a9651e61df8d561186930f0f5e729a89a
tdillon <tdillon@mit.edu>
2024-10-23 17:56:36 -0400

    getting there

--- src/gating_data_collection/src/gating_signal_visualizer.py
+++ src/gating_data_collection/src/gating_signal_visualizer.py
@@ -1,5 +1,5 @@
 import rospy
-from sensor_msgs.msg import Image
+from std_msgs.msg import Int32
 import numpy as np
 import cv2
 
@@ -7,28 +7,42 @@ class GatingVisualizer:
     def __init__(self):
         rospy.init_node('gating_signal_visualizer')
 
-        # Subscribe to the binary image topic
-        self.ecg_image_sub = rospy.Subscriber('/ecg_signal', Image, self.gating_callback)
+        # Parameters for visualization
+        self.signal_length = 100  # Width of the window in pixels
+        self.signal_height = 100  # Height of the window in pixels
+        self.signal_image = np.zeros((self.signal_height, self.signal_length, 3), dtype=np.uint8)
+
+        # Variable to hold the latest signal value
+        self.latest_signal_value = 0
+
+        # Subscribe to the binary signal topic
+        self.ecg_image_sub = rospy.Subscriber('/ecg', Int32, self.gating_callback)
+
+        # Set a timer to update the visualization at a fixed rate (e.g., 30 Hz)
+        update_rate = 30
+        self.timer = rospy.Timer(rospy.Duration(1.0 / 30), self.update_visualization)
+
+        print("Initialized")
 
     def gating_callback(self, msg):
-        # # Extract image data
-        # width = msg.width
-        # height = msg.height
+        # Store the latest value from the message
+        self.latest_signal_value = msg.data
 
-        # try:
-        #     # if binary
-        #     image_data = np.frombuffer(msg.data, dtype=np.uint8).reshape((-1, width))   
+    def update_visualization(self, event):
+        # Draw a white line for 1 and a black line for 0 based on the latest signal value
+        color = (255, 255, 255) if self.latest_signal_value == 1 else (0, 0, 0)
 
-        # except:
-        #     # if rgb
-        #     image_data  = np.frombuffer(msg.data, dtype=np.uint8).reshape((height, width, 3))
-        #     # image_data  = np.frombuffer(msg.data, dtype=np.uint8).reshape((height, -1)) 
+        # Shift the image left to make space for the new signal
+        self.signal_image[:, :-1] = self.signal_image[:, 1:]
 
-        # # Display the binary image
-        # cv2.imshow('Image', image_data)
-        cv2.waitKey(10)
-        pass
+        # Update the last column with the new signal value
+        self.signal_image[:, -1] = color
 
+        resized_image = cv2.resize(self.signal_image, (self.signal_length * 10, self.signal_height * 3))
+
+        # Display the image
+        cv2.imshow("ECG Signal", resized_image)
+        cv2.waitKey(1)
 
     def run(self):
         rospy.spin()
@@ -37,7 +51,10 @@ if __name__ == '__main__':
     visualizer = GatingVisualizer()
     try:
         visualizer.run()
+    
     except rospy.ROSInterruptException:
         pass
     finally:
         cv2.destroyAllWindows()
+
+    
